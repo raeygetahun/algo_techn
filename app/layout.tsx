48ed9609a00c54f072ce0e0a4e40b6d6298d36a1
raeygetahun <getahunraey@gmail.com>
2025-05-22 21:22:45 +0200

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/algo_techn/_next/static/media/image.6883cbde.png" sizes="any" />
+        <link rel="icon" href="/_next/static/media/image.6883cbde.png" sizes="any" />
         {/* You can add more favicon types if needed */}
         {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
       </head>
